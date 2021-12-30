e8c20738536cc4bd7e2e3da6c9165a20b211fdb9
Alexander Garuba <alexandergaruba96@gmail.com>
2021-12-30 23:58:24 +0100

    first commit

---
+++ src/components/UseMemoExample.jsx
@@ -0,0 +1,49 @@
+import { sqrt } from "prelude-ls";
+import { useState, useEffect, useRef, useMemo } from "react";
+
+function UseMemoExample() {
+  const [number, setNumber] = useState(1);
+  const [inc, setInc] = useState(0);
+
+  const sqr = useMemo(() => getSqrt(number), [number]);
+
+  const renders = useRef(1);
+
+  useEffect(() => {
+    renders.current = renders.current + 1;
+  });
+
+  const onClick = () => {
+    setInc((prevState) => {
+      return prevState + 1;
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="number"
+        className="form-control w-25"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <h2 className="my-3">
+        The sqrt of {number} is {sqr}
+      </h2>
+      <button className="btn btn-primary" onClick={onClick}>
+        Re Render
+      </button>
+      <h3>Renders {renders.current}</h3>
+    </div>
+  );
+}
+
+function getSqrt(n) {
+  for (let i = 0; i <= 10000; i++) {
+    console.log(i);
+  }
+  console.log("Expensive Function Called!");
+  return Math.sqrt(n);
+}
+
+export default UseMemoExample;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const [localStorageValue, setLocalStorageValue] = useState(() =>
+    getLocalStorageValue(key, initialValue)
+  );
+
+  const setValue = (value) => {
+    // CHeck if function
+    const valueToStore =
+      value instanceof Function ? value(localStorageValue) : value;
+
+    // set to State
+    setLocalStorageValue(value);
+
+    // Set to localStorage
+    localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
+
+  return [localStorageValue, setValue];
+}
+
+function getLocalStorageValue(key, initialValue) {
+  const itemFromStorage = localStorage.getItem(key);
+  return itemFromStorage ? JSON.parse(itemFromStorage) : initialValue;
+}
+
+export default useLocalStorage;
